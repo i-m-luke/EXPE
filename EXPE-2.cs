c5f37ae89b0e263035d2d3f924c56318c2a94e9f
i-m-luke <46042236+i-m-luke@users.noreply.github.com>
2026-09-03 21:29:15 +0200

    Update EXPE-2.cs

--- EXPE-2.cs
+++ EXPE-2.cs
@@ -1,27 +1,53 @@
-// MODEL:
+﻿﻿namespace Zat.SystemTest.Common.Net.MVVM.ViewModel.UserConfig;
 
-TestAssemblyEntity(
-    TestType TestType,
-    string Name,
-    string Path)
+using CommunityToolkit.Mvvm.DependencyInjection;
 
-TestAssembly(
-    TestSuite[] RuntimeTests,
-    TestSuite[] ApplicationTests)
-    
-TestSuite(
-    TestCase[] TestCases,
-    ...,
-    string Path) 
-    : TestAssemblyEntity(.., Path)
-    
-TestCase(...)
-    : TestAssemblyEntity(...)
+using Zat.SystemTest.Common.Net.MVVM.ViewModel;
 
-// API & LOGIC
-INunitTestRunnerProxy
-    RunTestAsync: 
-    - Bude brát TestAssemblyEntity[]
-    - Potřebuje znát pouze Path (podle toho se entita spustí )
-    - Nepotřebuje Children (vše dostane na hromadě, jak testcasy tak testsuites)
-    LoadAssemblyAsync: Navrátí TestAssemblyEntity
+/// <summary>
+/// Represents a test user config data container.
+/// </summary>
+/// <param name="model">The model.</param>
+/// <param name="ioc">Shared IoC.</param>
+public class TestUserConfig(
+    Model.UserConfig.TestUserConfig model,
+    Ioc ioc)
+    : ValidatableViewModelBase<Model.UserConfig.TestUserConfig>(model)
+{
+    /// <summary>
+    /// Gets the ID of the test the user config is associated with.
+    /// </summary>
+    public Guid TestId
+        => this.Model.TestId;
+
+    /// <summary>
+    /// Gets the test user config object.
+    /// </summary>
+    [field: AllowNull]
+    public TestConfigObject ConfigObject
+        => this.GetOrInitialize(ref field, () => 
+        {
+            var userConfigEditorService = new UserConfigEditorService();
+            var editorsServices = new EditorsServices
+                userConfigEditorServices: userConfigEditorService,
+                packageManifestEditorServices: null);
+                
+            // TODO: Extend ioc with EditorsServices
+            var configObject = new TestConfigObject(this.Model.ConfigObject, ioc));
+            
+            // TODO: Instantiate the configObject and provided it to the userConfigEditorService
+            
+            return configObject;
+        }
+}
+
+...
+
+public class UserConfigEditorService(Lazy<object> configInstanceLazy) : IUserConfigEditorService
+{
+    public object GetConfigObject()
+        => configInstanceLazy.Value;
+        
+    public object[] GetDriverConfigs()
+        => [];
+}
