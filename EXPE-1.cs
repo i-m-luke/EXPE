e2883e424740d703f26848539098e05f8ddca1d1
i-m-luke <kratochvil.luke@gmail.com>
2026-09-05 20:19:08 +0200

    update EXPE-1.cs

--- EXPE-1.cs
+++ EXPE-1.cs
@@ -1,19 +1,20 @@
-﻿protected static async Task<DataAnalysisCreateResult<TDocumentViewModel>> CreateWithDataAnalysis<
+﻿// CreateResult { ViewModel; ModelChanged }
+protected static async Task<CreateResult<TDocumentViewModel>> CreateWithDataAnalysis<
         TDocumentModel, TDocumentViewModel, TAnalyzer, TResolver, TAnalyzeResult>(
         TDocumentModel possibleModel,
         Func<TDocumentModel, TDocumentViewModel> viewModelFactory,
         Func<TAnalyzer> analyzerFactory,
         Func<TAnalyzeResult, TResolver> resolverFactory,
         Action<TDocumentModel>? postHook = null,
-        Func<string,string> onSevereConflictsMessage = null,
-        Func<string,string> onCriticalConflictsMessage = null)
+        Func<string,string> onDataHasSevereConflictsMessage = null,
+        Func<string,string> onDataHasCriticalConflictsMessage = null)
         where TDocumentModel : class, new()
         where TAnalyzer : IAnalyzer<TDocumentModel, TAnalyzeResult>
         where TResolver : DataAnalysisResolverBase<TDocumentModel, TAnalyzeResult>
         where TAnalyzeResult : AnalyzeResultBase
     {
-        onSevereConflicts ??= logText => TextUtils.SafeFormat(Properties.Errors.LoadedDataHasCriticalConflicts, logText); // TODO: Change resources text 
-        onCriticalConflicts ??= logText => TextUtils.SafeFormat(Properties.Errors.LoadedDataHasSevereConflicts, logText);
+        onDataHasCriticalConflictsMessage ??= logText => TextUtils.SafeFormat(Properties.Errors.LoadedDataHasCriticalConflicts, logText); // TODO: Change resources text 
+        onDataHasSevereConflictsMessage ??= logText => TextUtils.SafeFormat(Properties.Errors.LoadedDataHasSevereConflicts, logText);
         
         var analyzeResult = await AnalyzeAsync<TDocumentModel, TAnalyzer, TAnalyzeResult>(
             possibleModel, analyzerFactory);
@@ -44,11 +45,3 @@
             Create(resolvedModel, viewModelFactory, postHook),
             analyzeResult.HasConflicts); // If had conflicts then the model was changed at this point
     }
-
- 
-            // RemoteEditorVisitor bude mít metodu bool OnDataHasCriticalConflicts(string analyzeLogText)
-                
-            }
-
-            // RemoteEditorVisitor bude mít metodu bool OnDataHasSevereConflicts(string analyzeLogText)
-            
